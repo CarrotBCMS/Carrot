0a79f3611a453bd018000245291523d621c79b1b
Heiko Dreyer <mail@boxedfolder.com>
2015-06-05 12:59:42 +0200

    Removed several imports

--- core/src/main/java/com/boxedfolder/carrot/Application.java
+++ core/src/main/java/com/boxedfolder/carrot/Application.java
@@ -1,12 +1,8 @@
 package com.boxedfolder.carrot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Heiko Dreyer (heiko@boxedfolder.com)
