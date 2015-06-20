0d424e0d5fce29ef7a5086545d4b9b2158eb7460
Heiko Dreyer <mail@boxedfolder.com>
2015-06-20 19:28:56 +0200

    Added events list

--- client/app/scripts/controllers/event.js
+++ client/app/scripts/controllers/event.js
@@ -9,8 +9,42 @@
  * Controller showing beacons
  */
 angular.module('Carrot')
-    .controller('EventController', function ($scope, $log, Event) {
-        $scope.events = Event.query(function(data) {
-            $log.debug(JSON.stringify(data));
-        });;
+    .controller('EventController', function ($scope, $log, ngTableParams, Event, Entity) {
+        $scope.data = Event.query(function (data) {
+            $scope.tableParams = new ngTableParams({
+                page: 1,
+                count: 10
+            }, {
+                total: data.length,
+                getData: function ($defer, params) {
+                    var pageData = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    params.total(data.length);
+
+                    // Fix empty page after deletion
+                    if (pageData.length == 0 && data.length > 0) {
+                        pageData = data.slice((params.page() - 2) * params.count(), params.page() * params.count());
+                    }
+
+                    $defer.resolve(pageData);
+                }
+            });
+        });
+
+        $scope.delete = function (item) {
+            Entity.delete(item, "Event", function (object) {
+                var index = $scope.data.indexOf(item);
+                if (index > -1) {
+                    $scope.data.splice(index, 1);
+                }
+                $scope.tableParams.reload()
+            });
+        };
+
+        $scope.edit = function (item) {
+            $log.debug("Edit item " + item);
+        };
+
+        $scope.add = function () {
+            $log.debug("Add item");
+        };
     });
