cd02bcd3091cc69c973db66ef8f03832018cddaf
Heiko Dreyer <mail@boxedfolder.com>
2015-06-21 13:54:58 +0200

    More basic tests added

---
+++ client/test/spec/services/entity.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Service: EntityService', function () {
+    // load the service's module
+    beforeEach(module('Carrot'));
+
+    // instantiate service
+    var entity;
+    beforeEach(inject(function (_EntityService_) {
+        entity = _EntityService_;
+    }));
+
+    it('should get category methods', function () {
+        expect(entity).toBeDefined();
+        expect(entity.list).toBeDefined();
+        expect(entity.edit).toBeDefined();
+        expect(entity.delete).toBeDefined();
+    });
+});
