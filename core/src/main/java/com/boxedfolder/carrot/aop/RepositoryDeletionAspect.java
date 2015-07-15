835a49c92e76fe1951ec2ba7de0106a207169c85
Heiko Dreyer <mail@boxedfolder.com>
2015-07-15 09:54:24 +0200

    Updated deletion aspect

--- core/src/main/java/com/boxedfolder/carrot/aop/RepositoryDeletionAspect.java
+++ core/src/main/java/com/boxedfolder/carrot/aop/RepositoryDeletionAspect.java
@@ -20,18 +20,18 @@ import javax.inject.Inject;
 public class RepositoryDeletionAspect {
     private DeletionLogRepository logRepository;
 
-    @Pointcut("execution(* org.springframework.data.repository.CrudRepository+.delete(*))")
+    @Pointcut("execution(* com.boxedfolder.carrot.repository.OrderedRepository+.delete(*))")
     public void deletionMethod() {
     }
 
     @After("deletionMethod()")
-    public void logResourceMethod(JoinPoint joinPoint) {
+    public void logDeletionMethod(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         if (args[0] == null || !(args[0] instanceof AbstractEntity)) {
             return;
         }
 
-        // Create DeletionLog for deleted repository object
+        // Create DeletionLog for deleted entity
         AbstractEntity entity = (AbstractEntity)args[0];
         EntityDeletionLog deletionLog = new EntityDeletionLog();
         deletionLog.setType(entity.getClass());
