f40fe8fc169d7f739075b38c8db7e8f74a588d32
Heiko Dreyer <mail@boxedfolder.com>
2015-07-26 23:58:56 +0200

    Added error pages & proper settings

--- core/src/main/java/com/boxedfolder/carrot/Application.java
+++ core/src/main/java/com/boxedfolder/carrot/Application.java
@@ -2,7 +2,12 @@ package com.boxedfolder.carrot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author Heiko Dreyer (heiko@boxedfolder.com)
@@ -12,5 +17,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // Error pages for our application
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+                ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+                container.addErrorPages(error401Page, error404Page, error500Page);
+            }
+        };
     }
 }
