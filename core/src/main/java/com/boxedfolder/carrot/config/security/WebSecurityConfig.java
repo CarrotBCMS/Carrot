21444583f420a0ce38a561c0291bd8227c391004
Heiko Dreyer <mail@boxedfolder.com>
2015-11-08 12:52:16 +0100

    Fixed permission bug in log sending

--- core/src/main/java/com/boxedfolder/carrot/config/security/WebSecurityConfig.java
+++ core/src/main/java/com/boxedfolder/carrot/config/security/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/client/analytics/logs").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/client/analytics/logs/**").permitAll();
 
         // Define secured routes here
         String[] securedEndpoints = {
