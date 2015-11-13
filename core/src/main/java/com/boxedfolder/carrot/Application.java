f1194715d8435d905c6f418173a1959137780e56
Heiko Dreyer <mail@boxedfolder.com>
2015-11-13 23:26:29 +0100

    Added support for war deployment

--- core/src/main/java/com/boxedfolder/carrot/Application.java
+++ core/src/main/java/com/boxedfolder/carrot/Application.java
@@ -20,10 +20,12 @@ package com.boxedfolder.carrot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
@@ -32,8 +34,13 @@ import org.springframework.http.HttpStatus;
  */
 @EnableConfigurationProperties
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class); // This is necessary for war deployment
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
