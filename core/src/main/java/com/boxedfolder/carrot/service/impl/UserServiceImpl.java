34f180ef8210e033ffc1cf9c9c598ae1884e2e3f
Heiko Dreyer <mail@boxedfolder.com>
2015-12-02 21:47:28 +0100

    Added license comments

--- core/src/main/java/com/boxedfolder/carrot/service/impl/UserServiceImpl.java
+++ core/src/main/java/com/boxedfolder/carrot/service/impl/UserServiceImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Carrot - beacon management
+ * Copyright (C) 2015 Heiko Dreyer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.boxedfolder.carrot.service.impl;
 
 import com.boxedfolder.carrot.config.security.AuthenticationHelper;
@@ -26,7 +44,7 @@ public class UserServiceImpl extends CrudServiceImpl<User, UserRepository> imple
 
     @NotNull
     @Value("${app.baseUrl}")
-    private String baseUrl;
+    private String baseUrl = "http://app.carrot.re";
 
     @Override
     public User findByEmail(String email) {
