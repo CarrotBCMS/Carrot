2f114d914c7866127d3c313cbfd929a5e9de6779
Heiko Dreyer <mail@boxedfolder.com>
2015-12-13 16:02:58 +0100

    Fixed index.html

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -13,10 +13,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);          
-
-  // Define this for live reloading of different paths
-  var modRewrite = require('connect-modrewrite');
+  require('time-grunt')(grunt);
 
   // Configurable paths for the application
   var appConfig = {
